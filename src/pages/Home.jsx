8a9844ae48fc955df9b7770d2cfbe3b2e6d6d020
6suk <koandjo9116@gmail.com>
2023-06-27 15:37:20 +0900

    ✨ feat : 영화 데이터 불러오기 (#1)
    - 더보기 클릭 시 추가 데이터 받아오기
    - 메인페이지 컴포넌트 생성 및 Map 돌리기
    
    close #3, close #4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,68 @@
-import { useEffect } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMovies } from '../store/movieSlice';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  useEffect(() => {}, []);
+  const dispatch = useDispatch();
+  const [limit, setLimit] = useState(20);
+  const [loading, setLoading] = useState(true);
+  let { movies } = useSelector((state) => state);
+  movies = movies.data;
 
-  return <div></div>;
+  const handleMoreView = () => {
+    setLimit(limit + 10);
+  };
+
+  const getMovieData = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&limit=${limit}`
+      );
+      const movies = await data.data.movies;
+      dispatch(setMovies(movies));
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovieData();
+  }, [limit]);
+
+  return (
+    <section>
+      <div className="container">
+        <ul>{!loading && <MovieCards movies={movies} />}</ul>
+        <button
+          type="button"
+          onClick={() => {
+            handleMoreView();
+          }}>
+          더보기
+        </button>
+      </div>
+    </section>
+  );
 };
 
+const MovieCards = ({ movies }) => {
+  const nav = useNavigate();
+
+  return movies.map((movie) => (
+    <li key={movie.id} onClick={() => nav(`/detail/${movie.id}`)}>
+      <article>
+        <img src={movie.medium_cover_image} alt={movie.title} />
+        <p>
+          {movie.title}
+          <span>{movie.year}</span>
+        </p>
+        <p>{movie.rating}</p>
+        <p>{movie.summary}</p>
+      </article>
+    </li>
+  ));
+};
 export default Home;
